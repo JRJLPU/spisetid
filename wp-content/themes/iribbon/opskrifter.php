248a235ac9779c2f8bee1039faa7709087ce5d88
JRJLPU <JRJLPU@gmail.com>
2016-12-15 12:39:50 +0100

    site

---
+++ wp-content/themes/iribbon/opskrifter.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ Template Name: Opskrifter
+ */
+
+
+get_header(); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1>Opskrifter </h1>
+            <p class="lead">Her kan du finde alle sidens opskrifter...</p>
+            <div class="col-sm-10">
+                
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
---
+++ wp-content/themes/iribbon/opskriftsside.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  Template Name: Opskriftsside
+ */
+
+
+get_header();
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
+        <div class="col-sm-4" style="background-color:lavenderblush;">.col-sm-4</div>
+        <div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
+    </div> 
+</div>
+</div>
+<?php get_footer(); ?>
