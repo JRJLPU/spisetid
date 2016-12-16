a807ea54dfc3959328af445ea6417870fb7054fa
JRJLPU <JRJLPU@gmail.com>
2016-12-16 12:24:28 +0100

    bae

--- wp-content/themes/iribbon/opskriftsside.php
+++ wp-content/themes/iribbon/opskriftsside.php
@@ -7,11 +7,12 @@
 get_header();
 ?>
 <div class="container">
-    <div class="row">
-        <div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
-        <div class="col-sm-4" style="background-color:lavenderblush;">.col-sm-4</div>
-        <div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
-    </div> 
-</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">.col-sm-4</div>
+            <div class="col-sm-4">.col-sm-4</div>
+            <div class="col-sm-4">.col-sm-4</div>
+        </div>
+    </div>
 </div>
 <?php get_footer(); ?>
